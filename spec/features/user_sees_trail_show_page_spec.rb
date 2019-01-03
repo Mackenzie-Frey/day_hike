705b57bdc811821a3bcd8a6d77f248a294ee8239
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-01-02 18:00:00 -0700

    Completes User Story 9.

---
+++ spec/features/user_sees_trail_show_page_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'As a user on the trail show page' do
+  it 'I see all info' do
+    trip_1 = Trip.create(name: 'Beginner Trip', start_date: 25, end_date: 30)
+    trip_2 = Trip.create(name: 'Medium Salsa Trip', start_date: 25, end_date: 31)
+
+    trail_1 = trip_1.trails.create(name: 'Happy Trail', length: 3, address: '111 This Lane')
+
+
+    visit trail_path(trail_1)
+
+    expect(page).to have_content(trail_1.name)
+    expect(page).to have_content(trip_1.name)
+    expect(page).to have_content(trip_1.total_length)
+  end
+end
+
+
+
+# User Story 10 of 10
+#
+# As a visitor,
+# when I visit a trail's page,
+# I see the total number of hiking trips where this trail
+# was included
